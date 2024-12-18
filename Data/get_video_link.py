a0bc4972f72d39fb0845a675f10cfe4e06009189
Angel-dash <njlghmr@gmail.com>
2024-12-18 19:27:39 +0545

    video links added and loaded

--- Data/get_video_link.py
+++ Data/get_video_link.py
@@ -1,6 +1,7 @@
 import os
 import requests
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
 
@@ -23,16 +24,17 @@ def get_chanel_id(chanel_name):
     else:
         return None
 
+
 def get_video_links(channel_id):
     url = f"{BASE_URL}/channels/search"
     video_links = []
     next_page_token = None
     while True:
         params = {
-            "part":"snippet",
-            "channel_id":channel_id,
-            "maxResults":50,
-            "type":"video",
+            "part": "snippet",
+            "channel_id": channel_id,
+            "maxResults": 50,
+            "type": "video",
             "key": api_key
         }
         if next_page_token:
@@ -52,15 +54,36 @@ def get_video_links(channel_id):
     return video_links
 
 
+def save_video_links(video_links, filename="video_links.json"):
+    with open(filename, 'w') as file:
+        json.dump(video_links, file)
+    print(f"{len(video_links)} The video links is saved successfully to {filename}")
+
+
+def load_video_links(filename="video_links.json"):
+    try:
+        with open(filename, 'r') as file:
+            video_links = json.load(file)
+            print(f"{len(video_links)} The video links is loaded successfully from {filename}")
+            return video_links
+    except FileNotFoundError:
+        print(f"The file {filename} not found")
+        return []
+
+
 if __name__ == "__main__":
-    channel_name = input("Enter the YouTube channel name: ")
-    channel_id = get_chanel_id(channel_name)
+    video_links = load_video_links()
+    if video_links:
+        print(f"Using {len(video_links)} saved video links")
+    else:
+        channel_name = input("Enter the YouTube channel name: ")
+        channel_id = get_chanel_id(channel_name)
 
-    if channel_id:
-        print(f"Fetching videos for channel: {channel_name} (ID: {channel_id})")
-        video_links = get_video_links(channel_id)
-
-        print(f"Found {len(video_links)} videos.")
-        for link in video_links:
-            print(link)
-
+        if channel_id:
+            print(f"Fetching videos for channel: {channel_name} (ID: {channel_id})")
+            video_links = get_video_links(channel_id)
+            save_video_links(video_links)
+        else:
+            print("Failed to fetch video links")
+    for link in video_links:
+        print(link)
