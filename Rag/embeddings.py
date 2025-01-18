953b9c808005d7f8ab8e8ef178c60fbc24ea522b
Angel-dash <njlghmr@gmail.com>
2025-01-18 19:41:19 +0545

    changes made to how docs are loaded

--- Rag/embeddings.py
+++ Rag/embeddings.py
@@ -102,3 +102,20 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+chroma_client = chromadb.Client()
+collection = chroma_client.get_or_create_collection(name = "my_collection")
+collection.upsert(
+    documents = [
+        "This is a dcouments about pineapple",
+        "this is a document about oranges"
+    ],
+    ids = ['id1', 'id2']
+
+)
+results = collection.query(
+    query_texts=["This is a query document about florida"], # Chroma will embed this for you
+    n_results=2 # how many results to return
+)
+
+print(results)
